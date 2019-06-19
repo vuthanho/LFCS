cb424382508290b497d25a967fff46559d6a202b
vuthanho <o.vuthanh@orange.fr>
2019-06-19 17:34:59 +0200

    still initial

---
+++ addscript.m
@@ -0,0 +1,5 @@
+%% Add the paths corresponding to source code and resources
+clc;
+addpath( genpath('../openexr-matlab-master/'));
+addpath( genpath('src/') );
+run('../vlfeat-0.9.19/toolbox/vl_setup');
